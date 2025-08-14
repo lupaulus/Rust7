f711c893f3e0d649d9bf03f9e477798ba24319f3
Dave Nardella <davenardella@live.it>
2025-08-14 12:15:23 +0200

    updated

--- target/package/rust7-0.1.1/target/debug/deps/rust7-738d74281ad2648a.d
+++ target/package/rust7-0.1.1/target/debug/deps/rust7-738d74281ad2648a.d
@@ -1,8 +1,8 @@
-E:\GitHub\Rust7\target\package/rust7-0.1.1\target\debug\deps\rust7-738d74281ad2648a.d: src\lib.rs src\client.rs src\../README.md
+E:\GitHub\Rust7\target\debug\deps\rust7-738d74281ad2648a.d: src\lib.rs src\client.rs src\../README.md
 
-E:\GitHub\Rust7\target\package/rust7-0.1.1\target\debug\deps\librust7-738d74281ad2648a.rlib: src\lib.rs src\client.rs src\../README.md
+E:\GitHub\Rust7\target\debug\deps\librust7-738d74281ad2648a.rlib: src\lib.rs src\client.rs src\../README.md
 
-E:\GitHub\Rust7\target\package/rust7-0.1.1\target\debug\deps\librust7-738d74281ad2648a.rmeta: src\lib.rs src\client.rs src\../README.md
+E:\GitHub\Rust7\target\debug\deps\librust7-738d74281ad2648a.rmeta: src\lib.rs src\client.rs src\../README.md
 
 src\lib.rs:
 src\client.rs:
