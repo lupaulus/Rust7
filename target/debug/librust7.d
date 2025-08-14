17ffcf980a5245420e102cac83e28322d6ed1ff5
Dave Nardella <davenardella@live.it>
2025-08-14 11:01:57 +0200

    build

---
+++ target/debug/librust7.d
@@ -0,0 +1,1 @@
+E:\GitHub\Rust7\target\debug\librust7.rlib: E:\GitHub\Rust7\README.md E:\GitHub\Rust7\src\client.rs E:\GitHub\Rust7\src\lib.rs
--- target/package/rust7-0.1.1/target/doc/trait.impl/core/marker/trait.UnsafeUnpin.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["rust7",[["impl UnsafeUnpin for <a class=\"enum\" href=\"rust7/client/enum.S7Error.html\" title=\"enum rust7::client::S7Error\">S7Error</a>",1,["rust7::client::S7Error"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"rust7/client/struct.S7Client.html\" title=\"struct rust7::client::S7Client\">S7Client</a>",1,["rust7::client::S7Client"]]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[345]}
